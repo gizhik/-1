f3d2bde9d022c19b167688d8c7ac30f026fa343f
gizhik <106312935+gizhik@users.noreply.github.com>
2022-05-26 16:07:07 +0500

    Add files via upload

---
+++ домашка1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace домашка
+{
+	public struct EmptyStruct
+	{
+		int name;
+		float peaceOfCace;
+		bool barEntrance;
+		string fullName;
+		byte age;
+		short christBirth;
+		long peopleOnEarth;
+		uint dreamSalary;
+		ushort amountOfBoyfriends;
+		char emotion;
+	}
+}
+
